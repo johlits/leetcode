f9950b75f4b96d10ed6af62dbabaad06601e584f
Johan Litsfeldt <2831146+johlits@users.noreply.github.co>
2025-08-13 19:32:20 +0200

    Sudoku solver

---
+++ Hard/SudokuSolver.cs
@@ -0,0 +1,131 @@
+﻿
+namespace LeetCode.SudokuSolver
+{
+    public class Solution
+    {
+        public bool CheckRow(ref int[,] values, int y)
+        {
+            bool[] check = new bool[9];
+            for (var x = 0; x < 9; x++)
+            {
+                if (values[x, y] == 0)
+                {
+                    continue;
+                }
+                if (check[values[x, y] - 1])
+                {
+                    return false;
+                }
+                else {
+                    check[values[x, y] - 1] = true;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckCol(ref int[,] values, int x)
+        {
+            bool[] check = new bool[9];
+            for (var y = 0; y < 9; y++)
+            {
+                if (values[x, y] == 0)
+                {
+                    continue;
+                }
+                if (check[values[x, y] - 1])
+                {
+                    return false;
+                }
+                else
+                {
+                    check[values[x, y] - 1] = true;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckBox(ref int[,] values, int x, int y)
+        {
+            bool[] check = new bool[9];
+
+            var xbox = x / 3 * 3;
+            var ybox = y / 3 * 3;
+
+            for (var i = xbox; i < xbox + 3; i++)
+            {
+                for (var j = ybox; j < ybox + 3; j++)
+                {
+                    if (values[i, j] == 0)
+                    {
+                        continue;
+                    }
+                    if (check[values[i, j] - 1])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        check[values[i, j] - 1] = true;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private bool Solve(int[,] values)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    if (values[x, y] == 0)
+                    {
+                        for (int num = 1; num <= 9; num++)
+                        {
+                            values[x, y] = num;
+                            if (CheckRow(ref values, y) &&
+                                CheckCol(ref values, x) &&
+                                CheckBox(ref values, x, y))
+                            {
+                                if (Solve(values))
+                                {
+                                    return true;
+                                }
+                            }
+                        }
+                        values[x, y] = 0;
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void SolveSudoku(char[][] board)
+        {
+            int[,] values = new int[9, 9];
+
+            for (var x = 0; x < 9; x++)
+            {
+                for (var y = 0; y < 9; y++)
+                {
+                    if (board[x][y] != '.')
+                    {
+                        values[x, y] = board[x][y] - '0';
+                    }
+                }
+            }
+
+            Solve(values);
+
+            for (var x = 0; x < 9; x++)
+            {
+                for (var y = 0; y < 9; y++)
+                {
+                    board[x][y] = (char)('0' + values[x, y]);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,9 @@
-using LeetCode.ThreeSum;
+using LeetCode.SudokuSolver;
 
 var solution = new Solution();
 
-var a = solution.ThreeSum([-1,0,1,2,-1,-4]);
+char[][] board = [['5', '3', '.', '.', '7', '.', '.', '.', '.'], ['6', '.', '.', '1', '9', '5', '.', '.', '.'], ['.', '9', '8', '.', '.', '.', '.', '6', '.'], ['8', '.', '.', '.', '6', '.', '.', '.', '3'], ['4', '.', '.', '8', '.', '3', '.', '.', '1'], ['7', '.', '.', '.', '2', '.', '.', '.', '6'], ['.', '6', '.', '.', '.', '.', '2', '8', '.'], ['.', '.', '.', '4', '1', '9', '.', '.', '5'], ['.', '.', '.', '.', '8', '.', '.', '7', '9']];
 
-foreach (var b in a)
-{
-    foreach (var c in b)
-    {
-        Console.Write(c + " ");
-    }
-    Console.WriteLine();
-}
+solution.SolveSudoku(board);
 
 Console.WriteLine("");
